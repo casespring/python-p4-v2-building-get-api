8554d7df886a0b960bb5f2b60abc976669529e7e
Marco Upia <markremembers@gmail.com>
2024-01-17 15:52:26 -0500

    code-along

--- server/app.py
+++ server/app.py
@@ -19,9 +19,69 @@ db.init_app(app)
 def index():
     return "Index for Game/Review/User API"
 
-# start building your API here
+@app.route('/games')
+def games():
 
+    games = []
+    for game in Game.query.all():
+        # game_dict = {
+        #     "title": game.title,
+        #     "genre": game.genre,
+        #     "platform": game.platform,
+        #     "price": game.price,
+        # }
+        # games.append(game_dict)
+
+        # games = [game.to_dict() for game in Game.query.aall()] list comprehensions kind
+
+        game_dict = game.to_dict()
+        games.append(game_dict)
+
+    response = make_response(
+        # jsonify(games),
+        games,
+        200,
+        {'Content-Type':'application/json'}
+    )
+    return response
+
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    # game_dict = {
+    #     'title': game.title,
+    #     'genre': game.genre,
+    #     'platform': game.platform,
+    #     'price': game.price,
+    # }
+
+    game_dict = game.to_dict()
+
+    response = make_response(game_dict, 200)
+
+    return response
+
+@app.route('/games/users/<int:id>')
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    users = []
+    for review in game.reviews:
+        user = review.user
+        user_dict = user.to_dict(rules=("-reviews",))
+        users.append(user_dict)
+
+    # users = [review.user.to_dict(rules=('-reviews',)) list comprehension style
+    #          for review in game.reviews]
+        
+    # users = [user.to_dict(rules=("-reviews",)) for user in game.users] with association proxy
+
+    response = make_response(
+        users,
+        200
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
-
